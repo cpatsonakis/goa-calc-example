a98eef4c88753beae3141939ffba0d3b23de75d4
Christos Patsonakis <cpatsonakis@iti.gr>
2022-09-26 15:08:59 +0300

    Skeleton code for modifying at run-time Server URL in swagger UI by employing kin-openapi3 module. Also added Redoc UI. Still needs a bit of work...

--- dummy/cmd/demo/main.go
+++ dummy/cmd/demo/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
 
 	goahttp "goa.design/goa/v3/http"
@@ -13,6 +14,7 @@ import (
 
 	"github.com/cpatsonakis/goa-calc-example/gen/calc"
 	"github.com/cpatsonakis/goa-calc-example/gen/http/calc/server"
+	kin "github.com/getkin/kin-openapi/openapi3"
 	goa "goa.design/goa/v3/pkg"
 )
 
@@ -58,14 +60,17 @@ func main() {
 		handler = goahttpmiddle.Log(adapter)(handler)
 		handler = goahttpmiddle.RequestID()(handler)
 	}
+	someFunc()
 	swaggerUIStuff(mux)
+	redocUIStuff(mux)
 	dec := goahttp.RequestDecoder                        //# Set HTTP request decoder
 	enc := goahttp.ResponseEncoder                       //# Set HTTP response encoder
 	svr := server.New(endpoints, mux, dec, enc, eh, nil) //# Create Goa HTTP server
 	server.Mount(mux, svr)                               //# Mount Goa server on mux
-	httpsvr := &http.Server{                             //# Create Go HTTP server
-		Addr:    "localhost:9000", //# Configure server address
-		Handler: handler,          //# Set request handler
+	//externalURL := "http://chpats.dlt.iti.gr:9000"
+	httpsvr := &http.Server{ //# Create Go HTTP server
+		Addr:    ":9000", //# Configure server address
+		Handler: handler, //# Set request handler
 	}
 	fmt.Println("Paei na sikosei ton goa http server...")
 	if err := httpsvr.ListenAndServe(); err != nil { //# Start HTTP server
@@ -73,8 +78,38 @@ func main() {
 	}
 }
 
+func someFunc() {
+	doc, err := kin.NewLoader().LoadFromFile("openapi3.json")
+	if err != nil {
+		panic(err)
+	}
+	serverURL, err := url.Parse("/")
+	if err != nil {
+		panic(err)
+	}
+	server, strArray, str := doc.Servers.MatchURL(serverURL)
+	fmt.Printf("Server : %v\n", server)
+	fmt.Printf("strArray : %v\n", strArray)
+	fmt.Printf("str : %v\n", str)
+	server.URL = "http://chpats.dlt.iti.gr:9000"
+	bytesDoc, err := doc.MarshalJSON()
+	if err != nil {
+		panic(err)
+	}
+	newDocFile, err := os.OpenFile("./static/openapi3.json", os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		panic(err)
+	}
+	newDocFile.Write(bytesDoc)
+	newDocFile.Close()
+}
+
 func swaggerUIStuff(muxer goahttp.MiddlewareMuxer) {
 	fh := http.StripPrefix("/swaggerui/", http.FileServer(http.Dir("./static/swagger-ui")))
+	fhJSON := http.StripPrefix("/swaggerui/", http.FileServer(http.Dir("./static/")))
+	muxer.Handle(http.MethodGet,
+		"/swaggerui/openapi3.json",
+		fhJSON.ServeHTTP)
 	muxer.Handle(http.MethodGet,
 		"/swaggerui/{file}",
 		fh.ServeHTTP)
@@ -83,3 +118,36 @@ func swaggerUIStuff(muxer goahttp.MiddlewareMuxer) {
 		fh.ServeHTTP)
 
 }
+
+func redocUIStuff(muxer goahttp.MiddlewareMuxer) {
+	fh := http.StripPrefix("/redoc/", http.FileServer(http.Dir("./static/redoc")))
+	fhJSON := http.StripPrefix("/redoc/", http.FileServer(http.Dir("./static/")))
+	muxer.Handle(http.MethodGet,
+		"/redoc/openapi3.json",
+		fhJSON.ServeHTTP)
+	muxer.Handle(http.MethodGet,
+		"/redoc/{file}",
+		fh.ServeHTTP)
+	muxer.Handle(http.MethodGet,
+		"/redoc/",
+		fh.ServeHTTP)
+
+}
+
+// func swaggerUIStuff(muxer goahttp.MiddlewareMuxer) {
+// 	// h := swgui.NewHandler("My Api", "/openapi3.json", "/")
+// 	// swguiHandler := http.StripPrefix("/swaggerui/", h)
+// 	swguiHandler := swgui.NewHandler("My Api", "/openapi3.json", "/swaggerui/")
+// 	// muxer.Handle(http.MethodGet,
+// 	// 	"/swaggerui/{file}",
+// 	// 	swguiHandler.ServeHTTP)
+// 	muxer.Handle(http.MethodGet,
+// 		"/swaggerui/{file}",
+// 		swguiHandler.ServeHTTP)
+// 	muxer.Handle(http.MethodGet,
+// 		"/swaggerui/",
+// 		swguiHandler.ServeHTTP)
+// 	// muxer.Handle(http.MethodGet,
+// 	// 	"/swaggerui/",
+// 	// 	http.StripPrefix("/swaggerui/", swguiHandler).ServeHTTP)
+// }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/cpatsonakis/goa-calc-example
 go 1.16
 
 require (
-	github.com/gorilla/mux v1.8.0
+	github.com/getkin/kin-openapi v0.103.0
+	github.com/go-openapi/swag v0.22.3 // indirect
+	github.com/invopop/yaml v0.2.0 // indirect
 	goa.design/goa/v3 v3.8.5
 	goa.design/plugins/v3 v3.8.5
 )
